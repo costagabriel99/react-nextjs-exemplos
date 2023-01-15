6675c168489008e2e12c91de408105eea481854a
Gabriel Costa <22gabrielgc@gmail.com>
2023-01-15 13:19:24 -0300

    importado fonte e configurações do site

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,17 @@
+import Head from 'next/head'
+
 import '../src/style.css'
 
 function App ({ Component, pageProps }) {
-    return <Component {...pageProps}/>  
+    return (
+        <>
+        <Head>
+            <title>Titulo Padrão</title>
+        </Head>
+        <Component {...pageProps}/>
+        </>
+    )  
+
 }
 
 export default App
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,23 @@
+import Head from 'next/head'
+
 import Title from "../src/components/title/Title"
 import Subtitle from "../src/components/subtitle/Subtitle"
 
 function HomePage (){
     return (
-        <div>
-            <Title>Um titulo</Title>
-            <Subtitle text= 'Um subtitulo'/>
-            <p>Um paragrafo qualquer</p>
-            <Title>Outro titulo</Title>
-            <Subtitle/>
-            <p>Um paragrafo qualquer</p>
-        </div>
+        <>
+            <Head>
+                <Title>Titulo diferente da HomePage</Title>
+            </Head>
+            <div>
+               <Title>Um titulo</Title>
+               <Subtitle text= 'Um subtitulo'/>
+              <p>Um paragrafo qualquer</p>
+              <Title>Outro titulo</Title>
+              <Subtitle/>
+              <p>Um paragrafo qualquer</p>
+            </div>
+        </>
     )
 }
 
