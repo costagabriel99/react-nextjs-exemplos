b9dfb53c65bdd774caad3e8f67528124a33f2725
Gabriel Costa <22gabrielgc@gmail.com>
2023-01-20 12:24:31 -0300

    Criado Header

---
+++ pages/exemplo.jsx
@@ -0,0 +1,18 @@
+import Header from '../src/components/header/Header'
+import Title from '../src/components/title/Title'
+import Subtitle from '../src/components/subtitle/Subtitle'
+
+function ExemploPage () {
+    return (
+        <>
+            <Header>
+                <Title>
+                    Café Dev
+                </Title>
+                <Subtitle text="Melhor Café para o programador" />
+            </Header>
+        </>
+    )
+}
+
+export default ExemploPage
